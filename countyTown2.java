2a71608d8d1b5593d5bd1ec872663a52d2d5a9aa
AnibeA <80783166+AnibeA@users.noreply.github.com>
2023-05-01 11:09:59 +0100

    Create countyTown2.java

---
+++ countyTown2.java
@@ -0,0 +1,23 @@
+class CountyTowns{
+Set<Town> listTowns(County cty){
+//List towns in a given county (10 marks)
+}
+Set<County> counties(){
+//list all counties (5 marks)
+}
+Set<Town> listAllTowns(){
+//return list of all towns (5 marks)
+}
+public List<County> findCounty(Town tn){
+// find county or counties for a given town (10 marks)
+}
+public boolean containsTown(Town tn){
+//check if town recorded (5 marks)
+}
+public boolean containsCounty(County cty){
+//check if county recorded (5 marks)
+}
+public Map<Town,TreeSet<County>> mapTownToCounty(){
+//return a map that maps towns to counties (10 marks)
+}
+}
